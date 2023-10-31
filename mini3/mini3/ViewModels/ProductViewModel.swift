c0cd65f05dbad4e8d7076e9f8337517028ba3804
lucascavalherie <lucascavalherie01@gmail.com>
2023-10-31 15:01:35 -0300

    product functions

--- mini3/mini3/ViewModels/ProductViewModel.swift
+++ mini3/mini3/ViewModels/ProductViewModel.swift
@@ -15,10 +15,34 @@ class ProductViewModel: ObservableObject {
     let sharedUserDefaults = UserDefaults()
     let productKey = "products"
     
-    var orders : [ProductModel] = []
+    var products : [ProductModel] = []
     var currentProduct : ProductModel?
     
+    func sortProducts() {
+        products.sort()
+    }
     
+    func addProduct(name: String, observation: String?, priceBase: Float, createdAt: Date) {
+        products.append(ProductModel(name: name, observation: observation, priceBase: priceBase, createdAt: createdAt))
+        sortProducts()
+    }
+    
+    func editProduct(id: UUID, name: String, observation: String?, priceBase: Float, createdAt: Date) {
+        if let index = products.firstIndex(where: { $0.id == id }) {
+            products[index].name = name
+            products[index].observation = observation
+            products[index].priceBase = priceBase
+            products[index].createdAt = createdAt
+        }
+    }
+    
+    func getProduct(id: UUID) -> ProductModel? {
+        return products.filter{$0.id == id}.first
+    }
+    
+    func listProducts() -> [ProductModel] {
+        return products
+    }
     
 }
 
