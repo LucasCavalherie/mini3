6fb2c09e3ee7375979cdf38b23442b8994a293af
Gustavo Sena <7505977+gustavoghsena@users.noreply.github.com>
2023-11-08 16:11:27 -0300

    view da agenda

--- mini3/mini3/Views/Order/OrderListView.swift
+++ mini3/mini3/Views/Order/OrderListView.swift
@@ -11,15 +11,82 @@ import SwiftUI
 struct OrderListView: View {
     var body: some View {
         NavigationStack {
-            VStack {
-                NavigationLink {
-                    OrderView()
-                } label: {
-                    Text("Ver pedido")
+            ScrollView{
+                ZStack{
+                    VStack {
+                        ZStack {
+                            Image("Cremosinho_Tab")
+                                .resizable()
+                            
+                            HStack(alignment: .top) {
+                                Image("LogoPave")
+                                Spacer()
+                                Button(action: {
+                                    print("adicionar pedido")
+                                }, label: {
+                                    Image(systemName: "plus.app.fill")
+                                        .font(.title)
+                                        .foregroundStyle(.yellow)
+                                    
+                                })
+                                
+                            }
+                            .padding(.horizontal, 32)
+                        }
+                        Spacer()
+                        
+                        VStack(spacing: 8){
+                            HStack {
+                                Text("Encomendas próximas")
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                Spacer()
+                            }
+                            NavigationLink {
+                                OrderView()
+                            } label: {
+                                OrderListCardView()
+                            }
+                            
+                            NavigationLink {
+                                OrderView()
+                            } label: {
+                                OrderListCardView()
+                            }
+                            
+                            Spacer()
+                                .frame(height: 20)
+                            
+                            HStack {
+                                Text("Todas as encomendas")
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                Spacer()
+                                
+                                Button(action: {
+                                    print("sort ascendente/descendente")
+                                }, label: {
+                                    Image(systemName: "arrow.up.arrow.down.square")
+                                })
+                                
+                                Button(action: {
+                                    print("sort entrega, criaçao e nome")
+                                }, label: {
+                                    Image(systemName: "line.3.horizontal.decrease.circle")
+                                })
+                                
+                                
+                            }
+                            OrderListCardView()
+                            
+                            Spacer()
+                        }
+                        .padding(32)
+                    }
+                    
                 }
-                
             }
-            .padding()
+            .background(Color.gray)
         }
     }
 }
